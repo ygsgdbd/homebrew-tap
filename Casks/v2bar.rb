56b31595e5c8d5631b2b07ef088b0429cc6b2721
制杖 <ygsgdbd@163.com>
2024-12-13 12:18:57 +0800

    Add V2Bar cask for macOS

---
+++ Casks/v2bar.rb
@@ -0,0 +1,27 @@
+cask "v2bar" do
+  version "0.1.0"
+  sha256 "4dbb47e554929ca97843e81b94a5ead22b98a239bb92265240eb498737b62723"
+
+  url "https://github.com/ygsgdbd/V2Bar/releases/download/v#{version}/V2Bar.dmg"
+  name "V2Bar"
+  desc "V2Bar 是一个简洁优雅的 macOS 菜单栏应用，为你提供快捷的 V2EX 访问体验。"
+  homepage "https://github.com/ygsgdbd/V2Bar"
+
+  auto_updates false
+  depends_on macos: ">= :ventura"
+
+  app "V2Bar.app"
+
+  caveats <<~EOS
+    V2Bar is currently unsigned. You'll need to:
+    1. Right-click the app and select "Open"
+    2. Click "Open" in the dialog that appears
+    3. Go to System Settings > Privacy & Security and approve the app
+  EOS
+
+  zap trash: [
+    "~/Library/Application Support/V2Bar",
+    "~/Library/Preferences/top.ygsgdbd.V2Bar.plist",
+    "~/Library/Caches/top.ygsgdbd.V2Bar"
+  ]
+end 
