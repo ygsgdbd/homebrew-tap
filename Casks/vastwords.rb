4a3aa8a133ddc378d60db0720cd8a419ac114e67
制杖 <ygsgdbd@hotmail.com>
2024-12-22 18:45:13 +0800

    Update VastWords cask SHA256 checksum for version 0.1.0

--- Casks/vastwords.rb
+++ Casks/vastwords.rb
@@ -1,6 +1,6 @@
 cask "vastwords" do
   version "0.1.0"
-  sha256 "319a3debad9eb9ec3596705dfb9609ab7d3c28bd76613a33989fd754e67aebb8"
+  sha256 "e537d9b713019366b8a10f4afd37a4d66739c434a6e419f74f41802ff1e15a41"
 
   url "https://github.com/ygsgdbd/VastWords/releases/download/v#{version}/VastWords.dmg"
   name "VastWords"
